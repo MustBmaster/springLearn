e3f45bedaffbea0d755111a7168051dcb4ceb6e7
MustBmaster <minhmocprovdqq@gmail.com>
2024-08-15 23:05:10 +0700

    #4
    Thêm dependency cho dev tool
    Thêm 2 router

--- src/main/java/vn/hoidanit/laptopshop/HellowordController.java
+++ src/main/java/vn/hoidanit/laptopshop/HellowordController.java
@@ -2,12 +2,23 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
     public class HellowordController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
-        
+        return "Hello World from Spring Boot!, this is the 2nd time i save without restarting";
+    }
+    // khởi tạo 2 routes mới
+    @GetMapping("/user")
+    public String userPage() {
+        return "User page ";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin page";
     }
 }
